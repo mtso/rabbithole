8033001ccc5110a5237761621aacc3cbf3b81cd4
mtso <~@mtso.io>
2022-05-21 21:54:09 -0700

    Set config port nums

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub const REDIS_URL: &'static str = "redis://127.0.0.1:55009/";
-pub const RABBITMQ_URL: &'static str = "amqp://guest:guest@localhost:55006";
-pub const RETHINKDB_PORT: u16 = 55006;
+pub const REDIS_URL: &'static str = "redis://127.0.0.1:55020/";
+pub const RABBITMQ_URL: &'static str = "amqp://guest:guest@localhost:55010";
+pub const RETHINKDB_PORT: u16 = 55000;
 
